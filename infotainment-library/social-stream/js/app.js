d87459b128251c23a935fcf3814a3214253aa91e
giustyn <80340879+giustyn@users.noreply.github.com>
2021-12-21 13:13:49 -0500

    commit

--- infotainment-library/social-stream/js/app.js
+++ infotainment-library/social-stream/js/app.js
@@ -1,15 +1,47 @@
 (function () {
     let username = "Adrenaline Agency";
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
+    const isOverflown = ({
+        clientHeight,
+        scrollHeight
+    }) => scrollHeight > clientHeight
+    const resizeText = ({
+        element,
+        elements,
+        minSize = 1,
+        maxSize = 2.5,
+        step = .25,
+        unit = 'em'
+    }) => {
+        (elements || [element]).forEach(el => {
+            let i = minSize
+            let overflow = false
+
+            const parent = el.parentNode
+
+            while (!overflow && i < maxSize) {
+                el.style.fontSize = `${i}${unit}`
+                overflow = isOverflown(parent)
+
+                if (!overflow) i += step
+            }
+            // revert to last state where no overflow happened
+            el.style.fontSize = `${i - step}${unit}`
+        })
     }
 
-    function spotlightHashtags() {
-        var str = $('#messagetmp').html();
-        var edt = str.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>");
-        $('#message').html(edt);
-        $('#messagetmp').remove();
+    function getRandomInt(min, max) {
+        // return Math.floor(Math.random() * max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function spotlightHashtags(selector) {
+
+
+
+        // <span id="messagetmp" class="component"></span>
+        // var txt2 = $("<span></span>").text("Text.").attr('id', 'messagetmp').addClass('component');   // Create with jQuery
+
     }
 
     function cloneData() {
@@ -29,7 +61,8 @@
     }
 
     function appendContent(data) {
-        let index = getRandomInt(data.Items.length),
+
+        let index = getRandomInt(0, data.Items.length),
             imgUrl = data.Items[index].Images[0].Url,
             account = "@" + data.Items[index].User.Username,
             icon = data.Items[index].ProviderIcon,
@@ -48,36 +81,27 @@
         $('#socialicon').css('background-image', 'url(' + (icon) + ')');
         $('#profilename').text(username);
         $('#profileaccount').text(account);
-        $('#messagetmp').text(post);
+        $('#message').text(post);
         $('#posted').text(last);
-        spotlightHashtags();
 
-        const isOverflown = ({ clientHeight, scrollHeight }) => scrollHeight > clientHeight
-        const resizeText = ({ element, elements, minSize = 10, maxSize = 62, step = 1, unit = 'px' }) => {
-          (elements || [element]).forEach(el => {
-            let i = minSize
-            let overflow = false
-        
-                const parent = el.parentNode
-        
-            while (!overflow && i < maxSize) {
-                el.style.fontSize = `${i}${unit}`
-                overflow = isOverflown(parent)
-        
-              if (!overflow) i += step
-            }
-        
-            // revert to last state where no overflow happened
-            el.style.fontSize = `${i - step}${unit}`
-          })
+        function clonePost(selector) {
+            var tmp = selector + 'tmp';
+            var clone = $(selector).clone().attr('id', selector.replace('#', '') + 'tmp');
+            $(selector).parent().append(clone);
+
+            var str = $(tmp).html();
+            var edt = str.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>");
+            $(selector).html(edt);
+            $(tmp).remove();
         }
+        clonePost('#message');
 
         resizeText({
             elements: document.querySelectorAll('#message'),
-            step: 0.5
-          })
+            step: 0.25
+        })
 
-        console.log("characters:", post.length)
+        console.log(index, "characters:", post.length)
 
     }
 
@@ -99,4 +123,9 @@
             })
     }
     readJson();
+
+    $(window).resize(function () {
+        $("#log").append("<div>Handler for .resize() called.</div>");
+    });
+
 })(jQuery);
