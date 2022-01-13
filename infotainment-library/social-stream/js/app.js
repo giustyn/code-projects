5bb7905bb82cf327728509bc0e00fe3bcb5714fb
giustyn <80340879+giustyn@users.noreply.github.com>
2022-01-13 16:27:41 -0500

    commit

--- infotainment-library/social-stream/js/app.js
+++ infotainment-library/social-stream/js/app.js
@@ -12,7 +12,7 @@ $(function () {
         resizeText({
             elements: document.querySelectorAll(el),
             step: 0.1,
-            minSize: 1,
+            minSize: .5,
             maxSize: 2,
             unit: 'em'
         })
@@ -61,26 +61,31 @@ $(function () {
 
     function animateTemplate($container, $template, data, current) {
         const $clone = $template.clone();
+        let ProfileImageUrl = data.User.ProfileImageUrl;
 
+        // alert(!data.User.ProfileImageUrl)
         $clone.attr("id", current).css('z-index', current).removeClass('hidden');
-        $clone.find('#socialicon').css('background-image', 'url(' + (data.ProviderIcon) + ')');
+        $clone.find('#socialicon .icon').attr('src', data.ProviderIcon);
         $clone.find('#username').text(data.User.Name);
         $clone.find('#useraccount').text(data.User.Username);
-        $clone.find('#usericon').css('background-image', 'url(' + (data.User.ProfileImageUrl) + ')');
+        if (!data.User.ProfileImageUrl === true) {
+            ProfileImageUrl = "./img/adr-brandmark.jpg";
+        }
+        $clone.find('#usericon .icon').attr('src', ProfileImageUrl);
         $clone.find('#message').text(data.Content);
         $clone.find('#posted').text(data.DisplayTime);
         $clone.find('#media .video').attr('src', data.Images[0].Url);
         $clone.find('#media .photo').css('background-image', 'url(' + (data.Images[0].Url) + ')');
 
         $container.append($clone);
-
-        fitText('#message');
+        
         // isolateHashtag('#message'); // not working
         // $("#message").html($("#message").html().replace(/#([^ ]+)/g, "<span class='hashtag'>$1</span>"));
-        $('#message').html($("#message").text().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>"))
-
-        animateFeed();
+        // $('#message').html($("#message").text().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>"))
         
+        fitText('#message');
+        animateFeed();
+
         revealer();
 
         setTimeout(function () {
@@ -93,11 +98,11 @@ $(function () {
         let current = 0;
         const $template = $("article");
         const $container = $("#main");
-        
+
         console.log(current, feeds[current])
         animateTemplate($container, $template, feeds[current], current);
         current += 1;
-        
+
         setInterval(function () {
             console.log(current, feeds[current])
             animateTemplate($container, $template, feeds[current], current);
@@ -111,8 +116,8 @@ $(function () {
     function getData() {
         $.get(dataURI[1])
             .done(function (response) {
-                $.each(response.Items, function(i) {
-                    feeds.push(response.Items[i]);                    
+                $.each(response.Items, function (i) {
+                    feeds.push(response.Items[i]);
                 })
                 iterateAnimations();
                 // setData(response);
