3fa588e60782d4a2829b525c8a4d56df153d3258
giustyn <80340879+giustyn@users.noreply.github.com>
2022-01-12 23:54:54 -0500

    commit

--- infotainment-library/social-stream/js/app.js
+++ infotainment-library/social-stream/js/app.js
@@ -1,141 +1,123 @@
-(function () {
-    let username = "Adrenaline Agency";
+$(function () {
+    let username = "Adrenaline Agency",
+        dataURI = [
+            local = "c:\\data\\social\\social.json",
+            server = "https://kitchen.screenfeed.com/social/data/r4r9qm9zg5jb4hspckpqyqzwj.json"
+        ],
+        transition = true,
+        timerDuration = 5000,
+        speed = 1500;
 
-    let dataURI = [
-        local = "c:\\data\\social\\social.json",
-        server = "https://kitchen.screenfeed.com/social/data/r4r9qm9zg5jb4hspckpqyqzwj.json"
-    ];
-
-    function fitTextToParent(selector) {
-        var tmp = selector + 'tmp';
-        var clone = $(selector).clone().attr('id', selector.replace('#', '') + 'tmp');
-        $(selector).parent().append(clone);
-        // (^|\s)(#[a-z\d-]+)
-        var edt = $(tmp).text().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>");
-        $(selector).html(edt);
-        $(tmp).remove();
-
+    function fitText(el) {
         resizeText({
-            elements: document.querySelectorAll(selector),
+            elements: document.querySelectorAll(el),
             step: 0.1,
             minSize: 1,
-            maxSize: 2.25,
+            maxSize: 2,
             unit: 'em'
         })
     }
 
-    function revealer(mode) {
-
-        let enabled = true,
-            flow = ['revealer--left', 'revealer--right', 'revealer--top', 'revealer--bottom'],
-            shuffle = flow[Math.floor(Math.random() * flow.length)];
-
-        console.log(shuffle)
-        let $transition = $('.revealer');
-        let $speed = parseInt($(':root').css('--revealer-speed'));
-        if (enabled == true) {
-            $transition.show().addClass('revealer--animate ' + shuffle).delay($speed * 2).queue(function () {
-                $(this).removeClass('revealer--animate ' + shuffle).hide().dequeue();
-            });
-        }
+    function isolateHashtag(el) {
+        var tmp = el + 'tmp';
+        var clone = $(el).clone().attr('id', el.replace('#', '') + 'tmp');
+        $(el).parent().append(clone);
+        var edt = $(tmp).text().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>");
+        $(el).html(edt);
+        $(tmp).remove();
     }
 
-    function animateFeed(interval) {
-
+    function animateFeed() {
         let speed = 1000,
             animateIn = anime.timeline({
-                easing: 'easeOutSine',
-                loop: false,
-                autoplay: false,
-                duration: speed
-            })
-            .add({
                 easing: 'easeOutQuart',
-                targets: '#media',
-                opacity: [0, 1],
-            }, '+=' + speed)
+                duration: speed,
+                autoplay: false,
+                loop: false
+            })
             .add({
                 targets: '#content',
-                opacity: [0, 1],
+                // opacity: [0, 1],
                 delay: anime.stagger(50),
-                // translateX: ['-20%', '0%'],
-            }, '-=' + speed),
-
-            animateOut = anime.timeline({
-                easing: 'easeInSine',
-                loop: false,
-                autoplay: false,
-                duration: speed
-            })
-            .add({
-                targets: 'article',
-                opacity: [1, 0],
-                delay: anime.stagger(10),
+                translateY: [100, 0],
             })
         animateIn.play();
-        // revealer(shuffle);
     }
 
-    function cycleFeeds(data) {
+    function revealer() {
+        const $transition = $('.revealer'),
+            mode = [
+                'revealer--left',
+                'revealer--right',
+                'revealer--top',
+                'revealer--bottom'
+            ],
+            shuffle = mode[(Math.random() * mode.length) | 0]
+        // console.log(shuffle);
+
+        $transition.addClass('revealer--animate').addClass(shuffle).delay(speed).queue(function () {
+            $(this).removeClass('revealer--animate').removeClass(shuffle).dequeue();
+        });
+    }
+
+    function setData(data) {
         let feed = data.Items,
             $template = $('#feed'),
-            $container = $('#main'),
-            $clone = $template.clone(),
-            interval = 7000,
-            limit = 10,
-            index = 0;
-        $template.remove();
-        $.each(feed, function (i, el) {
-            let imgUrl = el.Images[0].Url,
-                account = "@" + el.User.Username,
-                icon = el.ProviderIcon,
-                post = el.Content,
-                last = el.DisplayTime;
+            $container = $('#main');
+            
+            $.each(feed, function (i, el) {
 
-            setTimeout(() => {
-                console.log(i, el);
-                $clone.attr('id', i);
-                $clone.find('#socialicon').css('background-image', 'url(' + (icon) + ')');
-                $clone.find('#profilename').text(username);
-                $clone.find('#profileaccount').text(account);
-                $clone.find('#message').text(post);
-                $clone.find('#posted').text(last);
-
-                function mediaVideoType() {
-                    if (imgUrl.includes(".mp4?") != true) return false
-                    $clone.find('#media video').attr('src', imgUrl).siblings().remove();
-                    return true
-                }
-                if (mediaVideoType() != true) {
-                    $clone.find('#media .photo').css('background-image', 'url(' + (imgUrl) + ')');
-                    $clone.find('#media video').remove();
-                };
-
-                // todos: fix fittexttoparent
-                fitTextToParent('#message');
-
+                let $clone = $template.clone();
+                $clone.attr('id', i).css('z-index', feed.length - i);
+                $clone.find('#socialicon').css('background-image', 'url(' + (el.ProviderIcon) + ')');
+                $clone.find('#username').text(el.User.Name);
+                $clone.find('#useraccount').text(el.User.Username);
+                $clone.find('#usericon').css('background-image', 'url(' + (el.User.ProfileImageUrl) + ')');
+                $clone.find('#message').text(el.Content);
+                $clone.find('#posted').text(el.DisplayTime);
+                $clone.find('#media .video').attr('src', el.Images[0].Url);
+                $clone.find('#media .photo').css('background-image', 'url(' + (el.Images[0].Url) + ')');
                 $container.append($clone);
-                animateFeed(interval);
-            }, i * interval);
-        });
+                console.log(i, el);
+                
+                // in
+                setTimeout(() => {
+                    fitText('#message');
+                    isolateHashtag('#message');
+                    revealer();
+                    // animateFreed();
+                }, i * timerDuration);
+
+                // out
+                setTimeout(() => {
+                    $clone.remove();
+                }, i * timerDuration + 1000);
+            });
+        $template.remove();
+    }
+
+    function iterate() {
+        let currentIndex = 0;
+        setInterval(() => {
+
+        }, timerDuration);
     }
 
     function getData() {
         $.get(dataURI[1])
-            .done(function (data) {
-                cycleFeeds(data);
+            .done(function (response) {
+                setData(response);
+                console.log("dataURI: " + dataURI[1]);
             })
             .always(function () {
                 // $bumper[0].addEventListener("timeupdate", videoTimeUpdate);
-                console.log("dataURI: " + dataURI[1]);
             });
     }
 
     function init() {
-        // loadVideo();
         getData();
     }
 
     init();
 
-})(jQuery);
+});
