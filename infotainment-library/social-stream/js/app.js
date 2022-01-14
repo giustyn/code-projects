5128556d11039c87af189ff140e49abfc596ca8d
giustyn <80340879+giustyn@users.noreply.github.com>
2022-01-14 00:30:05 -0500

    commit

--- infotainment-library/social-stream/js/app.js
+++ infotainment-library/social-stream/js/app.js
@@ -1,12 +1,14 @@
 $(function () {
-    const username = "Adrenaline Agency",
+    const userName = "Adrenaline Agency",
+        userIcon = "./img/adr-brandmark.jpg",
+        timerDuration = 8000,
+        animeDuration = 1000,
+        revealerSpeed = parseInt($(':root').css('--revealer-speed')),
+        feeds = [],
         dataURI = [
             local = "c:\\data\\social\\social.json",
             server = "https://kitchen.screenfeed.com/social/data/r4r9qm9zg5jb4hspckpqyqzwj.json"
-        ],
-        timerDuration = 10000,
-        speed = 1500,
-        feeds = [];
+        ];
 
     function fitText(el) {
         resizeText({
@@ -22,29 +24,12 @@ $(function () {
         // var tmp = el + 'tmp';
         // var clone = $(el).clone().attr('id', el.replace('#', '') + 'tmp');
         // $(el).parent().append(clone);
-        var edt = $(tmp).text().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>");
+        var edt = $(el).html().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>");
         $(el).html(edt);
-        $(tmp).remove();
+        // $(tmp).remove();
 
         // $("#message").html($("#message").html().replace(/#([^ ]+)/g, "<span class='hashtag'>$1</span>"));
         // $('#message').html($("#message").text().replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<span class='hashtag'>$2</span>"))
-    }
-
-    function animateFeed() {
-        let speed = 1000,
-            animateIn = anime.timeline({
-                easing: 'easeOutQuart',
-                duration: speed,
-                autoplay: false,
-                loop: false
-            })
-            .add({
-                targets: '#main',
-                opacity: [0, 1],
-                delay: anime.stagger(50),
-                translate3d: [100, 0, 0],
-            }, '+=1000')
-        animateIn.play();
     }
 
     function revealer() {
@@ -56,46 +41,65 @@ $(function () {
                 'revealer--bottom'
             ],
             shuffle = mode[(Math.random() * mode.length) | 0];
-
-        $transition.addClass('revealer--animate').addClass(shuffle).delay(speed * 2).queue(function () {
+        $transition.addClass('revealer--animate').addClass(shuffle).delay(revealerSpeed).queue(function () {
             $(this).removeClass('revealer--animate').removeClass(shuffle).dequeue();
         });
+    }
+    
+    function animateFeed($container) {
+        var animateIn = anime.timeline({
+                easing: 'easeOutQuart',
+                duration: animeDuration,
+                autoplay: false,
+                loop: false
+            })
+            .add({
+                begin: function(){
+                    revealer();
+                },
+            })
+            // .add({
+            //     targets: 'section > article > div',
+            //     opacity: [0, 1],
+            //     delay: anime.stagger(40),
+            //     translateX: [100, 0],
+            // })
+
+        animateIn.play();
     }
 
     function animateTemplate($container, $template, data, current) {
         const $clone = $template.clone();
-        let ProfileImageUrl = data.User.ProfileImageUrl;
+        let ProfileImageUrl = data.User.ProfileImageUrl,
+            ProfileUserName = data.User.Name;
+
         if (!data.User.ProfileImageUrl === true) {
-            ProfileImageUrl = "./img/adr-brandmark.jpg";
-            // ProfileImageUrl = "./img/default-icon.svg";
+            // use default instagram image & username
+            ProfileImageUrl = userIcon;
+            ProfileUserName = userName;
         }
 
-        // alert(!data.User.ProfileImageUrl)
-        $clone.attr("id", current).css('z-index', feeds.length-current).removeClass('hidden');
+        $clone.attr("id", current).css('z-index', feeds.length - current).removeClass('hidden');
         $clone.find('#socialicon .icon').attr('src', data.ProviderIcon);
-        $clone.find('#username').text(data.User.Name);
+        $clone.find('#username').text(ProfileUserName);
         $clone.find('#useraccount').text(data.User.Username);
         $clone.find('#usericon .icon').attr('src', ProfileImageUrl);
         $clone.find('#message').text(data.Content);
         $clone.find('#posted').text(data.DisplayTime);
         $clone.find('#media .video').attr('src', data.Images[0].Url);
         $clone.find('#media .photo').css('background-image', 'url(' + (data.Images[0].Url) + ')');
-
         $container.append($clone);
-                
-        fitText('#message');
-        // isolateHashtag('#message'); // not working
-        // animateFeed();
 
-        revealer();
+        fitText('#message');
+        isolateHashtag('#message'); // not working
+        animateFeed($container);
 
         setTimeout(function () {
             $clone.remove();
-        }, timerDuration + 500);
+        }, timerDuration + (revealerSpeed / 2));
     }
 
     function iterateAnimations() {
-        // console.warn(feeds[])
         let current = 0;
         const $template = $("article");
         const $container = $("#main");
@@ -104,7 +108,7 @@ $(function () {
         animateTemplate($container, $template, feeds[current], current);
         // current += 1;
         current = (current + 1) % feeds.length;
-        
+
         setInterval(function () {
             console.log(current, feeds[current])
             animateTemplate($container, $template, feeds[current], current);
