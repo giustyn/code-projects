c65d3b4a4aafd38e74018cf2e930028e6bd29f4a
giustyn <giustyn@me.com>
2023-07-25 12:57:23 -0400

    commit

--- infotainment-library/trivia/js/app.js
+++ infotainment-library/trivia/js/app.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
+  const countdownTimer = 15000;
 
   function setDateTime() {
     var renderTime = function () {
       var clock = new Date();
       // Set date via Vanilla.js
-      date.textContent = clock.toLocaleString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
+      date.textContent = clock.toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
       });
       // Set time via Vanilla.js
-      time.textContent = clock.toLocaleString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
+      time.textContent = clock.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
         // second: 'numeric',
-        hour12: true
+        hour12: true,
       });
     };
     renderTime();
@@ -23,11 +24,14 @@ $(document).ready(function () {
 
   function revealer(direction) {
     var animation = $("#transition");
-    var duration = parseInt($(':root').css('--revealer-speed'));
+    var duration = parseInt($(":root").css("--revealer-speed"));
     var effect = "revealer--animate" + " " + direction;
-    animation.addClass(effect).delay(duration).queue(function () {
-      $(this).removeClass(effect).dequeue();
-    });
+    animation
+      .addClass(effect)
+      .delay(duration)
+      .queue(function () {
+        $(this).removeClass(effect).dequeue();
+      });
   }
 
   function splitText(content) {
@@ -36,69 +40,83 @@ $(document).ready(function () {
   }
 
   function randomizeInt(num) {
-    // generate unique random numbers within a range and store them to an array 
+    // generate unique random numbers within a range and store them to an array
     var arr = [];
     while (arr.length < num) {
       var r = Math.floor(Math.random() * num) + 1;
       if (arr.indexOf(r) === -1) arr.push(r);
     }
-    return arr
+    return arr;
   }
 
   function animateCounter(element, timer, delay) {
     var limit = Math.round(timer / 1000);
     let speed = 200;
 
-    anime.timeline({
-        easing: 'easeInOutElastic(.5, 1)',
+    anime
+      .timeline({
+        easing: "easeInOutElastic(.5, 1)",
         duration: 1500,
       })
-      .add({
-        targets: '.question',
-        opacity: [0, 1],
-        scale: [0, 1],
-        translateX: ['100%', '0%'],
-      }, '-=500')
-      .add({
-        targets: '.choices .option',
-        delay: anime.stagger(speed),
-        opacity: [0, 1],
-        scale: [0, 1],
-        translateX: ['100%', '0%'],
-        translateY: ['100%', '0%'],
-      }, '-=1200')
-      .add({
-        targets: '.counter, .timer',
-        opacity: [0, 1],
-        scale: [2, 1],
-        // translateX: ['50%', '0%'],
-        rotateX: '180deg',
-        translateY: ['100%', '0%'],
-        delay: anime.stagger(speed),
-        endDelay: timer
-      }, '-=1000')
-      .add({
-        targets: '.choices .option',
-        delay: anime.stagger(speed),
-        opacity: [1, 0],
-        scale: [1, 0],
-      }, '-=1500');
+      .add(
+        {
+          targets: ".question",
+          opacity: [0, 1],
+          scale: [0, 1],
+          translateX: ["100%", "0%"],
+        },
+        "-=0"
+      )
+      .add(
+        {
+          targets: ".choices .option",
+          delay: anime.stagger(speed),
+          opacity: [0, 1],
+          scale: [0, 1],
+          translateX: ["100%", "0%"],
+          translateY: ["100%", "0%"],
+        },
+        "-=0"
+      )
+      .add(
+        {
+          targets: ".counter, .timer",
+          opacity: [0, 1],
+          scale: [2, 1],
+          // translateX: ['50%', '0%'],
+          rotateX: "180deg",
+          translateY: ["100%", "0%"],
+          delay: anime.stagger(speed),
+          endDelay: timer,
+        },
+        "-=0"
+      )
+      .add(
+        {
+          targets: ".choices .option",
+          delay: anime.stagger(speed),
+          opacity: [1, 0],
+          scale: [1, 0],
+        },
+        "-=1500"
+      );
 
-    anime.timeline({
+    anime
+      .timeline({
         loop: false,
         delay: timer,
       })
       .add({
-        targets: '.answer',
-        rotateX: '180deg',
+        targets: ".answer",
+        rotateX: "180deg",
         opacity: [0, 1],
-        endDelay: delay
+        endDelay: delay,
       });
 
     function countdownTimer() {
       var count = limit;
       var interval = setInterval(function () {
-        count--
+        count--;
         $(".timer").text(count);
         // Display 'count' wherever you want to display it.
         // console.log(limit, count)
@@ -112,11 +130,11 @@ $(document).ready(function () {
 
     function progressBar() {
       var animateProgress = anime({
-        targets: 'progress',
+        targets: "progress",
         value: [0, 100],
         autoplay: true,
-        easing: 'linear',
-        duration: timer
+        easing: "linear",
+        duration: timer,
       });
       // document.querySelector('body').onclick = animateProgress.restart;
       animateProgress.restart;
@@ -127,7 +145,7 @@ $(document).ready(function () {
   function buildAnimatedSlide(todo, index, timer, delay, template, container) {
     let clone = template.clone();
     let node = clone.get();
-    let $revealer = parseInt($(':root').css('--revealer-enabled'));
+    let $revealer = parseInt($(":root").css("--revealer-enabled"));
 
     clone.attr("id", index).removeClass("hidden");
     clone.find(".question").html(todo.question);
@@ -144,7 +162,7 @@ $(document).ready(function () {
 
     setTimeout(function () {
       clone.remove();
-    }, timer + delay)
+    }, timer + delay);
 
     if ($revealer != 0) {
       revealer("revealer--right");
@@ -152,49 +170,63 @@ $(document).ready(function () {
   }
 
   function animateContent() {
-    // let data = "http://kitchen.screenfeed.com/feed/m9z8nmyas3pd4qkrnj7te7ye0.json";
+    // let data = "http://kitchen.screenfeed.com/trivia/data/15hasrp0fg9a6mznq0h0t6255a.json";
     let data = "./data/trivia.json";
-    let timer = parseInt($(':root').css('--countdown-timer'));
+    let timer = countdownTimer;
+    // let timer = parseInt($(':root').css('--countdown-timer'));
     let delay = 4000;
 
-    $.ajax(data)
-      .done((todos) => {
-        let index = 0;
-        let limit = todos.length;
-        // let limit = 10; // max number of content items
-        let randomInt = randomizeInt(limit);
-        let template = $(".trivia");
-        let container = $(".container");
+    $.ajax(data).done((todos) => {
+      let index = 0;
+      let limit = todos.length;
+      // let limit = 10; // max number of content items
+      let randomInt = randomizeInt(limit);
+      let template = $(".trivia");
+      let container = $(".container");
 
-        console.log('Total Item(s):', todos.length);
-        console.log('Item Limit:', limit);
-        console.log('Timer:', timer);
+      console.log("Total Item(s):", todos.length);
+      console.log("Item Limit:", limit);
+      console.log("Timer:", timer);
 
-        anime.timeline({
-            targets: 'header *',
-            easing: 'easeInOutElastic(.5, 1)',
-            duration: 1000
-          })
-          .add({
-            opacity: [0, 1],
-            delay: anime.stagger(100),
-            // translateX: ['-100%', '0%'],
-            translateY: ['100%', '0%'],
-          }, '+=0')
+      // anime.timeline({
+      //     targets: 'header *',
+      //     easing: 'easeInOutElastic(.5, 1)',
+      //     duration: 1000
+      //   })
+      //   .add({
+      //     opacity: [0, 1],
+      //     delay: anime.stagger(100),
+      //     // translateX: ['-100%', '0%'],
+      //     translateY: ['100%', '0%'],
+      //   }, '+=0')
 
+      console.log(randomInt[index], todos[randomInt[index]]);
+      buildAnimatedSlide(
+        todos[randomInt[index]],
+        index,
+        timer,
+        delay,
+        template,
+        container
+      );
+      index++;
+      let intervalId = setInterval(function () {
         console.log(randomInt[index], todos[randomInt[index]]);
-        buildAnimatedSlide(todos[randomInt[index]], index, timer, delay, template, container);
+        buildAnimatedSlide(
+          todos[randomInt[index]],
+          index,
+          timer,
+          delay,
+          template,
+          container
+        );
         index++;
-        let intervalId = setInterval(function () {
-          console.log(randomInt[index], todos[randomInt[index]]);
-          buildAnimatedSlide(todos[randomInt[index]], index, timer, delay, template, container);
-          index++;
-          if (index >= limit) {
-            index = 0;
-            clearInterval(intervalId);
-          }
-        }, timer + delay);
-      });
+        if (index >= limit) {
+          index = 0;
+          clearInterval(intervalId);
+        }
+      }, timer + delay);
+    });
   }
 
   function init() {
